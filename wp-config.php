560dd63849cb0e3cfce00a9bac6f832d3c0939b6
Loris Barallini <baralliniloris@gmail.com>
2024-04-20 15:14:10 +0200

    ajout sql db

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'msp' );
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'UwUPassword69' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
